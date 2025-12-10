10cf573c663ccc11f2e690c8b983bc72ab34d06c
SALMA <idmhandsalma44@gmail.com>
2025-12-10 21:00:36 +0000

    initialisation

---
+++ src/main/java/com/salma/mini_projet_pharmacie/MiniProjetPharmacieApplication.java
@@ -0,0 +1,13 @@
+package com.salma.mini_projet_pharmacie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjetPharmacieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniProjetPharmacieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/salma/mini_projet_pharmacie/MiniProjetPharmacieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salma.mini_projet_pharmacie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProjetPharmacieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

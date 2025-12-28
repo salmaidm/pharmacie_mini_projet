ad919d96ae0bcecf1bf2b181d1c0a5383730cc71
MmError404 <hajarbenzakour6@gmail.com>
2025-12-28 16:52:14 +0100

    hajar

--- src/main/java/com/salma/mini_projet_pharmacie/pharmacie/exception/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.salma.mini_projet_pharmacie.pharmacie.exception;
-
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
